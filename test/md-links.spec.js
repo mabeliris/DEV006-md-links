a87963100c099ad6f2f94e307cb20e0c247838e4
mabeliris <mabelle7623@gmail.com>
2023-07-21 09:48:46 -0400

    README

--- test/md-links.spec.js
+++ test/md-links.spec.js
@@ -32,7 +32,7 @@ describe("getFilesInDirectory",()=>{
 
 
 describe("mdLinks", () => {
-  it("deberia retornar una proomesa que se resuelve con un array de objetos", () => {
+  it("deberia retornar una promesa que se resuelve con un array de objetos", () => {
    const result =mdLinks(filePath, options);
     expect(result).resolves.toEqual([
         {
@@ -42,44 +42,8 @@ describe("mdLinks", () => {
           status: 200,
           statusText: "OK",
         },
-        /*{
-          texto: "Markdown",
-          href: "https://es.wikipedia.org/wiki/Markdown",
-          path: "C:\\Users\\mabelle\\DEV006-md-links\\prueba.md",
-          status: 200,
-          statusText: "OK",
-        },*/
+        
       ]) 
   });
 });
 
-/*describe("mdLinks", () => {
-  it("should return promise that resolves as an array", (done) => {
-    mdLinks(filePath, options)
-      .then((result) => {
-        console.log("la ruta es válida:", true); // La ruta existe y es válida
-        expect(result).toEqual([
-          {
-            texto: "Markdown",
-            href: "https://es.wikipedia.org/wiki/Markdown",
-            path: "C:\\Users\\mabelle\\DEV006-md-links\\prueba.md",
-            status: 200,
-            statusText: "ok",
-          },
-          {
-            texto: "Markdown",
-            href: "https://es.wikipedia.org/wiki/Markdown",
-            path: "C:\\Users\\mabelle\\DEV006-md-links\\prueba.md",
-            status: 404,
-            statusText: "fail",
-          },
-          // ... tus resultados esperados aquí ...
-        ]);
-        done(); // la prueba asíncrona ha finalizado correctamente.
-      })
-      .catch((error) => {
-        console.log("la ruta es válida:", false); // La ruta no es válida
-        done(error); // Si hay un error, pásalo como argumento a done.
-      });
-  });
-});*/
